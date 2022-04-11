bb3d9db1dc44a230fe84fc041af057f8783e8071
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-11 19:17:17 +0100

    [Feature]: adding script that exports a CSV file with employee info\

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,17 +6,29 @@ import sys
 
 
 if __name__ == "__main__":
-        url = 'https://jsonplaceholder.typicode.com/'
+    url = 'https://jsonplaceholder.typicode.com/'
 
-            userid = sys.argv[1]
-                user = '{}users/{}'.format(url, userid)
-                    res = requests.get(user)
-                        json_o = res.json()
-                            name = json_o.get('username')
+    userid = sys.argv[1]
+    user = '{}users/{}'.format(url, userid)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
 
-                                todos = '{}todos?userId={}'.format(url, userid)
-                                    res = requests.get(todos)
-                                        tasks = res.json()
-                                            l_task = []
-                                                for task in tasks:
-                                                            l_task.append([userid,
+    todos = '{}todos?userId={}'.format(url, userid)
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        l_task.append([userid,
+                       name,
+                       task.get('completed'),
+                       task.get('title')])
+
+    filename = '{}.csv'.format(userid)
+    with open(filename, mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file,
+                                     delimiter=',',
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_ALL)
+        for task in l_task:
+            employee_writer.writerow(task)
