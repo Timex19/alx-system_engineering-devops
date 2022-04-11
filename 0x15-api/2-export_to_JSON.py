1916403e708c69a5834f02dedc44d8c936993045
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-11 19:16:29 +0100

    [Feature]: adding script that exports a JSON file with employee info

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,17 +6,25 @@ import sys
 
 
 if __name__ == "__main__":
-        url = 'https://jsonplaceholder.typicode.com/'
+    url = 'https://jsonplaceholder.typicode.com/'
 
-            userid = sys.argv[1]
-                user = '{}users/{}'.format(url, userid)
-                    res = requests.get(user)
-                        json_o = res.json()
-                            name = json_o.get('username')
+    userid = sys.argv[1]
+    user = '{}users/{}'.format(url, userid)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
 
-                                todos = '{}todos?userId={}'.format(url, userid)
-                                    res = requests.get(todos)
-                                        tasks = res.json()
-                                            l_task = []
-                                                for task in tasks:
-                                                            dict_task = {"task": task.get('title'),
+    todos = '{}todos?userId={}'.format(url, userid)
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        dict_task = {"task": task.get('title'),
+                     "completed": task.get('completed'),
+                     "username": name}
+        l_task.append(dict_task)
+
+    d_task = {str(userid): l_task}
+    filename = '{}.json'.format(userid)
+    with open(filename, mode='w') as f:
+        json.dump(d_task, f)
