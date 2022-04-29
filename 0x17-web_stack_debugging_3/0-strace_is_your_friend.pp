fe381f5ece73678ac5e76b33835f3891ca23371f
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-29 08:49:47 +0100

    [Fix]: find apache error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-#Wordpress debugging
-
-exec { 'error conf php':
-  provider => 'shell',
-    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+# Puppet automated fix for wordpress
+exec { 'Debugging':
+  command  => 'sudo sed -i \'s/phpp/php/g\' /var/www/html/wp-settings.php && sudo service apache2 restart',
+  provider => shell,
 }
